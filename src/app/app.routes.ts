95b0b3ebcb23cf9e8962ef5fb82c8b949128e7b9
svdt <sebastian.vonderthannen@gmail.com>
2024-05-06 12:25:16 +0200

    removes Kontakte component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component'
-import { KontakteComponent } from './kontakte/kontakte.component';
 import { SurveysComponent } from './surveys/surveys.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent }, // Index route
-    { path: 'kontakte', component: KontakteComponent },
     { path: 'surveys', component: SurveysComponent },
     { path: 'about', component: AboutComponent },
     
